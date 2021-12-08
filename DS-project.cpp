ac9944a3b5fe0ed28117260c2da232f9530347ea
Thanatos095 <mahad095@gmail.com>
2021-12-08 16:31:18 +0500

    First actual commit

--- DS-project.cpp
+++ DS-project.cpp
@@ -1,8 +1,37 @@
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+#include <string>
 #include <iostream>
-#include <filesystem>
+#include <experimental/filesystem>
+#include <fstream>
+#include<unordered_map>
+#include<vector>
+
+using namespace std;
+
+namespace fs = experimental::filesystem;
+
+class Video
+{
+public:
+    string id;
+    string title;
+    int categoryId;
+    vector<string> comments;
+    
+    static Video&& CreateVideo(fs::path& p)
+    {
+        Video vid;
+        fstream obj(p);
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    return 0;
+    string path = "2016";
+    vector<Video> videos(200000);
+
+    for (auto& p : fs::recursive_directory_iterator(path))
+        cout << p.path() << endl;
+
+
 }
